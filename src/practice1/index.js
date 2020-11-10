504f167a1bc3ad43f4693251e9b63cd1b0ea8fc0
Yanqin Shen <yanqin.shen@thoughtworks.com>
2020-11-10 22:23:03 +0800

    [syq] feat: complete all practice

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,10 @@
+import { ADDRESS, PORT, PATH, REGION } from './constant'
+import getURL from './utils'
 
+const url = getURL(ADDRESS, PORT, PATH);
+const region = REGION
+
+export {
+  url,
+  region
+}
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,16 @@
-export const getPoetry
+export const getPoetry = () => {
+  return fetch('https://v1.jinrishici.com/all.json')
+    .then(res => {
+      if(res.status == 200) {
+        return res.json()
+      }
+      return Promise.reject()
+    })
+    .then(data => {
+      const { origin, author, content } = data;
+      return [origin, author, content];
+    })
+    .catch(err => {
+      console.log(err)
+    })
+} 
