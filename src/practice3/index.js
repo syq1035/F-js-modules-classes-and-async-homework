497b1818ac162ea13ea4c2c57762688e8c99b1cb
Yanqin Shen <yanqin.shen@thoughtworks.com>
2020-11-10 22:29:50 +0800

    [syq] feat: praction3 use async/await

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,16 +1,27 @@
-export const getPoetry = () => {
-  return fetch('https://v1.jinrishici.com/all.json')
-    .then(res => {
-      if(res.status == 200) {
-        return res.json()
-      }
-      return Promise.reject()
-    })
-    .then(data => {
-      const { origin, author, content } = data;
-      return [origin, author, content];
-    })
-    .catch(err => {
-      console.log(err)
-    })
+// export const getPoetry = () => {
+//   return fetch('https://v1.jinrishici.com/all.json')
+//     .then(res => {
+//       if(res.status == 200) {
+//         return res.json()
+//       }
+//       return Promise.reject()
+//     })
+//     .then(data => {
+//       const { origin, author, content } = data;
+//       return [origin, author, content];
+//     })
+//     .catch(err => {
+//       console.log(err)
+//     })
+// } 
+
+export const getPoetry = async () => {
+  const res = await fetch('https://v1.jinrishici.com/all.json')
+  if(res.status == 200) {
+    const { origin, author, content } = await res.json();
+    return [origin, author, content];
+  } else {
+    return new Error("error")
+  }
 } 
+
